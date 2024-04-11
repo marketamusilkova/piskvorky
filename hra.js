984acf8d2abf610b720015d013f6ed47213664ff
Markéta Musílková <mrs.musilkova@gmail.com>
2024-04-11 14:22:16 +0200

    Prohození obrázků znázorňujících, kdo je na tahu + odsazení hracího pole od spodu stránky

--- hra.js
+++ hra.js
@@ -1,48 +1,48 @@
 let currentPlayer = 'circle';
 
-const addCircle = (e) => {
+const addIcon = (e) => {
   e.target.disabled = true;
   if (currentPlayer === 'circle') {
     e.target.classList.add('playground__square--circle');
     currentPlayer = 'cross';
-    document.getElementById('who').src = 'pictures/circle.svg';
+    document.getElementById('who').src = 'pictures/cross.svg';
   } else {
     e.target.classList.add('playground__square--cross');
     currentPlayer = 'circle';
-    document.getElementById('who').src = 'pictures/cross.svg';
+    document.getElementById('who').src = 'pictures/circle.svg';
   }
 };
 
 document
   .querySelector('button:nth-child(1)')
-  .addEventListener('click', addCircle);
+  .addEventListener('click', addIcon);
 document
   .querySelector('button:nth-child(2)')
-  .addEventListener('click', addCircle);
+  .addEventListener('click', addIcon);
 document
   .querySelector('button:nth-child(3)')
-  .addEventListener('click', addCircle);
+  .addEventListener('click', addIcon);
 document
   .querySelector('button:nth-child(4)')
-  .addEventListener('click', addCircle);
+  .addEventListener('click', addIcon);
 document
   .querySelector('button:nth-child(5)')
-  .addEventListener('click', addCircle);
+  .addEventListener('click', addIcon);
 document
   .querySelector('button:nth-child(6)')
-  .addEventListener('click', addCircle);
+  .addEventListener('click', addIcon);
 document
   .querySelector('button:nth-child(7)')
-  .addEventListener('click', addCircle);
+  .addEventListener('click', addIcon);
 document
   .querySelector('button:nth-child(8)')
-  .addEventListener('click', addCircle);
+  .addEventListener('click', addIcon);
 document
   .querySelector('button:nth-child(9)')
-  .addEventListener('click', addCircle);
+  .addEventListener('click', addIcon);
 document
   .querySelector('button:nth-child(10)')
-  .addEventListener('click', addCircle);
+  .addEventListener('click', addIcon);
 
 document.querySelector('.button_restart').addEventListener('click', (e) => {
   if (!confirm('Opravdu chceš začít hrát znovu?')) {
